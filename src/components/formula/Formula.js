d563a4b35e0f16f55eb31521d24f10a6fd615252
Vladimir <angara99@gmail.com>
2020-05-20 22:48:30 +0300

    Finished big job on table logic

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -6,6 +6,7 @@ export class Formula extends ExcelComponent {
     super($root, {
       name: 'Formula',
       listeners: ['input', 'keydown'],
+      subscribe: ['currentText'],
       ...options,
     });
   }
@@ -21,16 +22,12 @@ export class Formula extends ExcelComponent {
     this.$formula = this.$root.find('#formula');
 
     this.$on('table:select', ($cell) => {
-      this.$formula.text($cell.text());
+      this.$formula.text($cell.data.value);
     });
+  }
 
-    this.$on('table:input', ($cell) => {
-      this.$formula.text($cell.text());
-    });
-
-    this.$subscribe((state) => {
-      console.log('Formula state : ', state);
-    });
+  storeChanged({ currentText }) {
+    this.$formula.text(currentText);
   }
 
   onInput(event) {
--- src/components/table/table.tempate.js
+++ src/components/table/table.tempate.js
@@ -1,34 +1,62 @@
+import { toInlineStyles } from '@core/utils';
+import { defaultStyles } from '@/constants';
+import { parse } from '@core/parse';
 const CODES = {
   A: 65,
   Z: 90,
 };
+const DEFAULT_WIDTH = 120;
+const DEFAULT_HEIGHT = 24;
 
-// function createCell(_, col) {
-// }
-//
-function toCell(row) {
+function getWidth(state, index) {
+  return (state[index] || DEFAULT_WIDTH) + 'px';
+}
+function getHeight(state, index) {
+  return (state[index] || DEFAULT_HEIGHT) + 'px';
+}
+
+function toCell(state, row) {
   return function (_, col) {
-    return `<div class="cell"
-    contenteditable
-    data-type="cell"
-    data-col="${col}"
-    data-id="${row}:${col}"
-      >123</div>`;
+    const width = getWidth(state.colState, col);
+    const id = row + ':' + col;
+    const data = state.dataState[id];
+    const styles = toInlineStyles({
+      ...defaultStyles,
+      ...state.stylesState[id],
+    });
+    return `<div
+              class="cell"
+              contenteditable
+              spellcheck="false"
+              data-type="cell"
+              data-col="${col}"
+              data-id="${id}"
+              data-value="${data || ''}"
+              style="${styles}; width: ${width}"
+            >${parse(data) || ''}</div>`;
   };
 }
 
-function createCol(col, index) {
-  return `<div class="column" data-type="resizable" data-col="${index}">
+function createCol({ col, index, width }) {
+  return `<div
+            class="column"
+            data-type="resizable"
+            data-col="${index}"
+            style="width: ${width}"
+            >
             ${col}
             <div class="col-resize" data-resize="col"></div>
           </div>`;
 }
 
-function createRow(index, string) {
+function createRow(index, string, state) {
   const resizer = index
     ? '<div class="row-resize" data-resize="row"></div>'
     : '';
-  return `<div class="row" data-type="resizable">
+  const height = getHeight(state, index);
+  return `<div class="row" data-type="resizable" data-row="${index}"
+            style="height: ${height}"
+          >
             <div class="row-info">
               ${index ? index : ''}
               ${resizer}
@@ -41,21 +69,32 @@ function toChar(_, index) {
   return String.fromCharCode(CODES.A + index);
 }
 
-export function createTable(rowCount = 15) {
+function withWidthFrom(state) {
+  return function (col, index) {
+    return {
+      col,
+      index,
+      width: getWidth(state, index),
+    };
+  };
+}
+
+export function createTable(rowCount = 15, state = {}) {
   const colsCount = CODES.Z - CODES.A + 1;
   const rows = [];
 
   const cols = new Array(colsCount)
     .fill('')
     .map(toChar)
+    .map(withWidthFrom(state.colState))
     .map(createCol)
     .join('');
 
-  rows.push(createRow(null, cols));
+  rows.push(createRow(null, cols, {}));
 
   for (let row = 0; row < rowCount; row++) {
-    const cols = new Array(colsCount).fill('').map(toCell(row)).join('');
-    rows.push(createRow(row + 1, cols));
+    const cols = new Array(colsCount).fill('').map(toCell(state, row)).join('');
+    rows.push(createRow(row + 1, cols, state.rowState));
   }
   return rows.join('');
 }
